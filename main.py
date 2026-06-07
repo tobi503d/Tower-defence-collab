8f68cd4a9275c0d8e9be7a54f094ed19e03d0766
Tobias H Larsen <tobias@hessnerlarsen.dk>
2026-06-08 00:31:40 +0200

    Added enemy stats and enemy creation function

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ import random
 
 pygame.init()
 
+#       --- Variables ---
+
+#   dictionary
+
+enemyStats = {
+    "basic": {
+        "hp": 10,
+        "speed": 10,
+        "defence": 10
+    },
+    "fast": {
+        "hp": 10,
+        "speed": 20,
+        "defence": 5
+    },
+    "tank": {
+        "hp": 15,
+        "speed": 5,
+        "defence": 20
+    }
+}
+
+#   lists
+
+enemyList = []
+
+
 icon = pygame.image.load("Images/Logo.png")
 
 screen = pygame.display.set_mode((1280, 720))
@@ -13,35 +40,43 @@ pygame.display.set_icon(icon)
 
 
 
+
+
+
+
+
+
+#      --- blueprint for enemies ---
+class enemy:
+    def __init__(self, ID_, HP_, speed_, defense_):
+        self.ID = ID_
+        self.HP = HP_
+        self.speed = speed_
+        self.defence = defense_
+    
+    def __repr__(self):
+        return f"Enemy(ID={self.ID}, HP={self.HP}, speed={self.speed}, defence={self.defence})"
+
+
+def enemyObjectMaker(enemyType):
+    
+    try:
+        tempID = enemyList[-1].ID +1
+    except:
+        tempID = 1
+
+    tempObjekt = enemy(tempID, enemyStats[enemyType]["hp"], enemyStats[enemyType]["speed"], enemyStats[enemyType]["defence"])
+    enemyList.append(tempObjekt)
+
+
+
+
+
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()
-
-#       --- all of the above is just to keep the game running ---
-
-
-buttonName = image()
-buttonName.button(2, 3, image)
-buttonName.button(2, 3, 43, 54, 223)
-
-#      --- blueprint for enemies ---
-class enemy:
-    def __init__(self, HP_, speed_, defense_):
-        self.HP = HP_
-        self.speed = speed_
-        self.defence = defense_
-
-
-#       ---enemies---
-basicEnemy = enemy(10, 10, 10)
-fastEnemy = enemy(5, 20, 10)
-slowEnemy = enemy(20, 5, 10)
-
-
-
-
-
-
+pygame.quit()
